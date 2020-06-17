4e899bc568d935f2b566e818ffca3b4e97b80605
momoKr <playmomo71@gmail.com>
2020-06-17 19:20:07 +0900

    h2 database, Jpa 적용

--- src/main/java/com/sample/web/UserController.java
+++ src/main/java/com/sample/web/UserController.java
@@ -3,27 +3,34 @@ package com.sample.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.sample.domain.User;
+import com.sample.domain.UserRepository;
+
 @Controller
 public class UserController {
 	private List<User> users = new ArrayList<User>();
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@PostMapping("/create")
 	public String create(User user) {
-		
+
 		System.out.println("user : " + user);
 		users.add(user);
-
+		userRepository.save(user);
 		return "redirect:/list";
 	}
-	
+
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+		model.addAttribute("users", userRepository.findAll());
 		return "list";
 	}
 }
