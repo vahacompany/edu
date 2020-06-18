869640113f127429e6287b33b5b503110b8ac8f0
momoKr <playmomo71@gmail.com>
2020-06-18 14:38:18 +0900

    put 오류 수정

--- src/main/java/com/sample/web/UserController.java
+++ src/main/java/com/sample/web/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sample.domain.User;
@@ -43,7 +42,7 @@ public class UserController {
 		userRepository.save(user);
 		return "redirect:/users";
 	}
-	@PutMapping("/{id}")
+	@PostMapping("/{id}")
 	public String update(@PathVariable Long id, User newUser) {
 		User user = userRepository.findById(id).get();
 		user.update(newUser);
