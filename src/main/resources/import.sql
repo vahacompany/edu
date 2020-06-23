ec0f41ff1e9725cfc883015a84c3686e1eba9418
momoKr <playmomo71@gmail.com>
2020-06-23 14:34:45 +0900

    이클립스에서 커밋!!

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,10 @@
-insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE) values (1, 'vaha1','1234','바하1','vaha1@email.com', CURRENT_TIMESTAMP());
-insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE) values (2, 'vaha2','1234','바하2','vaha2@email.com', CURRENT_TIMESTAMP());
-insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE) values (3, 'vaha3','1234','바하3','vaha3@email.com', CURRENT_TIMESTAMP());
-insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE) values (4, 'vaha4','1234','바하4','vaha4@email.com', CURRENT_TIMESTAMP());
-insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE) values (5, 'vaha5','1234','바하5','vaha5@email.com', CURRENT_TIMESTAMP());
+insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE)
+values (1, 'vaha1','1234','바하1','vaha1@email.com', CURRENT_TIMESTAMP());
+insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE)
+values (2, 'vaha2','1234','바하2','vaha2@email.com', CURRENT_TIMESTAMP());
+insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE)
+values (3, 'vaha3','1234','바하3','vaha3@email.com', CURRENT_TIMESTAMP());
+insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE)
+values (4, 'vaha4','1234','바하4','vaha4@email.com', CURRENT_TIMESTAMP());
+insert into user(id, USER_ID, password, USER_NAME, email, CREATE_DATE)
+values (5, 'vaha5','1234','바하5','vaha5@email.com', CURRENT_TIMESTAMP());
